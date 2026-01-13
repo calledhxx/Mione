56d58b6a8f6784971ed31f6a1d62f18eba515ed6
calledhxx <calledhxx@gmail.com>
2026-01-14 00:37:44 +0800

    Mione Still Building THIS IS NOT A COMPLETED FILE

--- HeadFile/SVV.c
+++ HeadFile/SVV.c
@@ -7,22 +7,61 @@
 
 HeadFunctionRespondObj SVV(const HeadFunctionRequestObj * HeadCallObjectPointer)
 {
+     HeadFunctionRespondObj Result = {0};
     const HeadFunctionRequestObj HeadCallObject = *HeadCallObjectPointer;
 
-    const PASSENGERRespondObj PassengersRes = PASSENGERS((PASSENGERRequestObj){
+    const unsigned int PairsSize = HeadCallObject.Train.CarriageLen;
+    const CarriageObj * Pairs = HeadCallObject.Train.Carriages;
+
+    ValueObjCarrier RESOURCEHeadSuffix = {0};
+    VariableObjPtrCarrier CONTAINERHeadSuffix = {0};
+
+    unsigned int Registration = 0;
+
+    for (unsigned int PairIndex = 0; PairIndex < PairsSize; PairIndex++)
+    {
+        const CarriageObj Pair = Pairs[PairIndex];
+        const PASSENGERRespondObj PassengersRes = PASSENGERS((PASSENGERRequestObj){
             .EventTemplate = HeadCallObject.EventTemplate,
-            .PassenegrsCarrier = HeadCallObject.Train.Carriages[0].CarriagePassengersCarrier
+            .PassenegrsCarrier = Pair.CarriagePassengersCarrier
         });
-    const MioneObjCarrier Passengers = PassengersRes.MioneCarrier;
+        const MioneObjCarrier Passengers = PassengersRes.MioneCarrier;
 
+        switch (Pair.CarriageManager.ObjType)
+        {
+        case HEAD:
+            {
+                RESOURCEHeadSuffix = RESOURCE((RESOURCERequestObj){
+                    .MioneCarrier = Passengers,
+                    .EventTemplate = HeadCallObject.EventTemplate
+                }).ValueCarrier;
 
-    const ValueObjCarrier a = RESOURCE((RESOURCERequestObj){
+                break;
+            }
+        case PROMPT:
+            {
+                switch (Pair.CarriageManager.Prompt.Identification)
+                {
+                case PROMPT_SET:
+                    {
+                        CONTAINERHeadSuffix = CONTAINER((CONTAINERRequestObj){
                             .MioneCarrier = Passengers,
                             .EventTemplate = HeadCallObject.EventTemplate
-                        }).ValueCarrier;
+                        }).VariablePtrCarrier;
 
+                        CONTAINERHeadSuffix.Carrier[0] -> Value
+                    }
+                default: exit(4);
 
+                }
 
+                Registration |= 1<<(Pair.CarriageManager.Prompt.Identification-1);
 
+                break;
+            }
+
+        default: exit(3);
+        }
+    }
     return (HeadFunctionRespondObj){0};
 }
